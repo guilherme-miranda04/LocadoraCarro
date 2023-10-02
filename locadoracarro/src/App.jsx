11ca703f8bd4ea186026aa13fa67d98ce07995a3
Guilherme Miranda <74930707+zGuilhermeTM@users.noreply.github.com>
2023-10-02 15:17:22 -0300

    Update App.jsx

--- locadoracarro/src/App.jsx
+++ locadoracarro/src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import { AppRoutes } from "./routes/index.jsx";
+
+import "react-toastify/dist/ReactToastify.css";
+import './App.css';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+        <AppRoutes />
+        <ToastContainer />
+    </BrowserRouter>
   )
 }
 
