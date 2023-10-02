361ad760a811b40979e055fd587856f6600d28ff
Guilherme Miranda <74930707+zGuilhermeTM@users.noreply.github.com>
2023-10-02 11:59:01 -0300

    Criação de Novas Páginas

--- locadoracarro/src/pages/Login/Login.jsx
+++ locadoracarro/src/pages/Login/Login.jsx
@@ -11,71 +11,71 @@ import './Login.css';
 
 
 function Login() {
-    const label = { inputProps: { "aria-label": "Checkbox demo" } };
-    return (
-        <div className="container_login">
-            <div className="form_image">
-                <img src={login} alt="imgWelcome" />
-            </div>
-            <div className="form">
-                <form /* onSubmit={handleSubmit} */>
-                    <div className="form_header">
-                        <div className="title">
-                            <h1>LOGIN</h1>
-                        </div>
-                    </div>
-                    <div className="input_box">
-                        <label htmlFor="email">E-mail</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Informe seu e-mail"
-                            required
-                        /* onChange={handleChangeEmail} */
-                        />
-                    </div>
+   const label = { inputProps: { "aria-label": "Checkbox demo" } };
+   return (
+      <div className="container_login">
+         <div className="form_image">
+            <img src={login} alt="imgWelcome" />
+         </div>
+         <div className="form">
+            <form /* onSubmit={handleSubmit} */>
+               <div className="form_header">
+                  <div className="title">
+                     <h1>LOGIN</h1>
+                  </div>
+               </div>
+               <div className="input_box">
+                  <label htmlFor="email">E-mail</label>
+                  <input
+                     type="email"
+                     id="email"
+                     name="email"
+                     placeholder="Informe seu e-mail"
+                     required
+                  /* onChange={handleChangeEmail} */
+                  />
+               </div>
 
-                    <div className="input_box">
-                        <label htmlFor="password">Senha</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Informe sua senha"
-                            required
-                        /* onChange={handleChangePassword} */
-                        />
-                    </div>
+               <div className="input_box">
+                  <label htmlFor="password">Senha</label>
+                  <input
+                     type="password"
+                     id="password"
+                     name="password"
+                     placeholder="Informe sua senha"
+                     required
+                  /* onChange={handleChangePassword} */
+                  />
+               </div>
 
-                    <div className="continue_button">
-                        <button /* onClick={handleSubmit} */>Logar
-                        <Link className="teste" to="/home"></Link>
-                        </button>
-                    </div>
-                    <br></br>
-                    <div className="textfield_remember">
-                        <FormControlLabel
-                            value="start"
-                            control={<Checkbox {...label} size="small" />}
-                            label="Mostrar Senha"
-                        />
-                    </div>
-                    <br></br>
-                    <br></br>
-                    <div className="textfield_createcount">
-                        <p>
-                            Ainda não tem conta?{" "}
-                            <Link className="hiperlink" to="/register">
-                                {" "}
-                                Criar Conta
-                            </Link>
-                        </p>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+               <div className="continue_button">
+                  <button /* onClick={handleSubmit} */>Logar
+                     <Link className="teste" to="/home"></Link>
+                  </button>
+               </div>
+               <br></br>
+               <div className="textfield_remember">
+                  <FormControlLabel
+                     value="start"
+                     control={<Checkbox {...label} size="small" />}
+                     label="Mostrar Senha"
+                  />
+               </div>
+               <br></br>
+               <br></br>
+               <div className="textfield_createcount">
+                  <p>
+                     Ainda não tem conta?{" "}
+                     <Link className="hiperlink" to="/register">
+                        {" "}
+                        Criar Conta
+                     </Link>
+                  </p>
+               </div>
+            </form>
+         </div>
+      </div>
+   )
 }
 
 export default Login;
