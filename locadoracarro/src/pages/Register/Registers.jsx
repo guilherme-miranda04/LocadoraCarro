2c50e57c7d46bd0b8dce2fb070d5c9db213c4231
Guilherme Miranda <74930707+zGuilhermeTM@users.noreply.github.com>
2023-10-02 15:09:25 -0300

    .

---
+++ locadoracarro/src/pages/Register/Registers.jsx
@@ -0,0 +1,16 @@
+import SideBar from "../../components/SideBar"
+
+import "../Home/Home.css";
+
+function Registers() {
+   return (
+      <div className="container_home">
+         <SideBar />
+         <main>
+            <h1>Clientes</h1>
+         </main>
+      </div>
+   );
+}
+
+export default Registers;
