5b02820199b0f026823df297a78de271150898a0
Guilherme Miranda <74930707+zGuilhermeTM@users.noreply.github.com>
2023-10-03 13:16:16 -0300

    .

--- locadoracarro/src/pages/Register/Registers.jsx
+++ locadoracarro/src/pages/Register/Registers.jsx
@@ -1,16 +1,17 @@
 import SideBar from "../../components/SideBar"
 
-import "../Home/Home.css";
+import styles from "./Registers.module.css";
 
 function Registers() {
-   return (
-      <div className="container_home">
-         <SideBar />
-         <main>
-            <h1>Clientes</h1>
-         </main>
-      </div>
-   );
+  return (
+    <div className={styles.container_registers}>
+      <SideBar />
+      <main>
+        <h1>Clientes</h1>
+        
+      </main >
+    </div >
+  );
 }
 
 export default Registers;
--- locadoracarro/src/pages/Vehicles/Vehicles.jsx
+++ locadoracarro/src/pages/Vehicles/Vehicles.jsx
@@ -1,10 +1,10 @@
 import SideBar from "../../components/SideBar"
 
-import "../Home/Home.css";
+import "./Vehicles.css";
 
 function Vehicles() {
    return (
-      <div className="container_home">
+      <div className="container_vehicles">
          <SideBar />
          <main>
             <h1>Veículos</h1>
